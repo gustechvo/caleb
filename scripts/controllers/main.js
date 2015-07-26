aaec652cf753f47e6d2cda5d7736fdc155e11cbb
gustechvo <talk@gustavomelendez.com>
2015-07-26 15:13:02 -0400

    Added Jribbble
    Added jribble.js and updated gitignore

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -11,17 +11,34 @@ angular.module('caleb').controller('MainCtrl', function ($scope,$rootScope, Load
         $rootScope.portfolioTypes = result.portfolio_types;
         console.log("postData",result);
         $rootScope.$broadcast('dataFetched');
-        //Load Instagram
-    var feed = new Instafeed({
-        get: 'user',
-        userId: 34215985,
-        accessToken: '',
-        template: '<a href="{{link}}"><img class="instaimage" src="{{image}}" /></a>',
-        sortBy: 'most-recent',
-        limit: 3,
-        resolution: "low_resolution"
-    });
-    feed.run();
+        
+        //Load Instagram Feed
+        var feed = new Instafeed({
+            get: 'user',
+            userId: 34215985,
+            accessToken: '34215985.467ede5.990958bb2ecf400eaaa7237728b3a5a7',
+            template: '<a href="{{link}}"><img class="instaimage" src="{{image}}" /></a>',
+            sortBy: 'most-recent',
+            limit: 3,
+            resolution: "low_resolution"
+        });
+        feed.run();
+
+        //Load Dribble Feed
+        $.jribbble.setToken('a96d5c9e21930f4ab63da82a48f0705df031f7193c72e1d4fc8ed1270a090906');
+
+        $.jribbble.users('calebheisey').shots({per_page: 2}).then(function(shots) {
+          var html = [];
+          
+          shots.forEach(function(shot) {
+            html.push('<a href="' + shot.html_url + '"><img class="dribbbleimage" src="' + shot.images.normal + '" /></a>');
+          });
+          
+          $('.shots').html(html.join(''));
+        });
+
+
+
 
     });
   }
